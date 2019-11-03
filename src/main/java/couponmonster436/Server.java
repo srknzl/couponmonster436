4266b980f2f8a9ac0acec4b70b8f66b92f8c0030
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-11-03 21:29:44 +0300

    s

--- src/main/java/couponmonster436/Server.java
+++ src/main/java/couponmonster436/Server.java
@@ -1,22 +1,25 @@
 package couponmonster436;
-import java.io.IOException;
-import java.io.BufferedReader;
+import java.io.*;
 import java.net.*;
-import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Random;
+
 
 public class Server {
     private ServerSocket serverSocket;
     Thread serverThread = null;
+    Thread producerThread = null;
     public static final int SERVERPORT = 6000;
+    private static HashMap<Integer,String> coupons;
+    private static int counter = 0;
 
     public Server(){
+        coupons = new HashMap<>();
         this.serverThread = new Thread(new ServerThread());
         this.serverThread.start();
+        this.producerThread = new Thread(new ProducerThread());
+        this.producerThread.start();
         System.out.println("Listening incoming connections on external ip of this device, port 6000...");
-    }
-
-    public void interruptThread(){
-        this.serverThread.interrupt();
     }
 
     class ServerThread implements Runnable {
@@ -49,12 +52,16 @@ public class Server {
         private Socket clientSocket;
 
         private BufferedReader input;
+        private BufferedWriter output;
+        private int lastLength = 0;
+
 
         public CommunicationThread(Socket clientSocket) {
 
             this.clientSocket = clientSocket;
             try {
                 this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+                this.output = new BufferedWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -64,12 +71,67 @@ public class Server {
             while (!Thread.currentThread().isInterrupted()) {
                 try {
                     String read = input.readLine();
-                    System.out.println(read);
+                    if(read == null) {
+                        System.out.println("Disconnected");
+                        return;
+                    }
+                    if(read.equals("coupons")){
+
+                    }else if(read.equals("select coupon")){
+
+                    }else if(read.equals("answer coupon")){
+
+                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+        public void informNewCoupons(){
+            int newSize = coupons.size();
+            if(lastLength == newSize){
+                return;
+            }
+            for(int i = lastLength; i < newSize;i++){
+                try {
+                    this.output.write(coupons.get(i));
+                    this.output.newLine();
+                    this.output.flush();
+                }catch (IOException e1){
+                    e1.printStackTrace();
+                }
+            }
+        }
 
+    }
+    class ProducerThread implements Runnable{
+        public String generate() {
+            int leftLimit = 97; // letter 'a'
+            int rightLimit = 122; // letter 'z'
+            int targetStringLength = 30;
+            Random random = new Random();
+            StringBuilder buffer = new StringBuilder(targetStringLength);
+            for (int i = 0; i < targetStringLength; i++) {
+                int randomLimitedInt = leftLimit + (int)
+                        (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            return buffer.toString();
+        }
+        @Override
+        public void run() {
+            while(true){
+                String token = generate();
+                System.out.println(token);
+                try {
+                    Thread.sleep((Math.round(Math.random()*30000))); // 0 to 30 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                coupons.put(counter,token);
+                counter++;
+            }
+        }
     }
 }
