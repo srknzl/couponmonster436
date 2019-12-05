5fd69b6fdcd17e7573da33e4d0ba38355d28fd7a
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-12-05 22:28:04 +0300

    Change lock

--- src/main/java/couponmonster436/Lock.java
+++ src/main/java/couponmonster436/Lock.java
@@ -1,16 +1,16 @@
 package couponmonster436;
 
-public class Lock { // used for mutual exclusion
+class Lock { // used for mutual exclusion
     private boolean value;
     Lock(boolean initValue) {
         value = initValue;
     }
-    public synchronized boolean P() { // atomic operation // blocking
+    synchronized boolean P() { // atomic operation // blocking
         if(!value)return false;
         value = false;
         return true;
     }
-    public synchronized void V() { // atomic operation // non-blocking
+    synchronized void V() { // atomic operation // non-blocking
         value = true;
     }
 }
