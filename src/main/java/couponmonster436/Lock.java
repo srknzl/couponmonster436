90b9643a29cdd0a29fdd4490a28efcfd9a6727da
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-12-05 22:34:17 +0300

    Add console logs

--- src/main/java/couponmonster436/Lock.java
+++ src/main/java/couponmonster436/Lock.java
@@ -6,11 +6,17 @@ class Lock { // used for mutual exclusion
         value = initValue;
     }
     synchronized boolean P() { // atomic operation // blocking
-        if(!value)return false;
+        if(!value){
+            System.out.println("P called, value is false return false");
+            return false;
+        }
+        System.out.println("P called, value is true");
         value = false;
+        System.out.println("P called, value is now false return true");
         return true;
     }
     synchronized void V() { // atomic operation // non-blocking
+        System.out.println("V is called value is now true");
         value = true;
     }
 }
