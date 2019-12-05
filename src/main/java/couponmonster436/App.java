c55c4db045ee866ae49e888315725f4ec8319e0a
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-12-05 22:38:14 +0300

    Add console logs

--- src/main/java/couponmonster436/App.java
+++ src/main/java/couponmonster436/App.java
@@ -103,7 +103,10 @@ class CommunicationThread implements Runnable {
                 pulseCounter = (pulseCounter+1)%10;
                 if(pulseCounter == 0)out.println("9");
                 if(out.checkError() || Thread.interrupted()){
-                    if(selectedCoupon!=null && selectedCoupon.lock != null)selectedCoupon.lock.V();
+                    if(selectedCoupon!=null && selectedCoupon.lock != null){
+                        System.out.println("V in pulse");
+                        selectedCoupon.lock.V();
+                    }
                     for (User nextUser : App.users) {
                         if (nextUser.username.equals(this.username)) {
                             App.users.remove(nextUser);
@@ -145,7 +148,10 @@ class CommunicationThread implements Runnable {
             } catch (IOException ef) {
                 ef.printStackTrace();
             }
-            if(selectedCoupon != null && selectedCoupon.lock != null)selectedCoupon.lock.V();
+            if(selectedCoupon != null && selectedCoupon.lock != null){
+                System.out.println("Lock in catch");
+                selectedCoupon.lock.V();
+            }
             App.Communications.removeElementAt(App.Communications.indexOf(this));
         }
     }
@@ -222,7 +228,10 @@ class CommunicationThread implements Runnable {
                     Coupon c = App.coupons.get(hash);
                     out.println("3No|" + hash);
                     System.out.println("Wrong answer");
-                    if (c != null && c.lock != null) c.lock.V();
+                    if (c != null && c.lock != null){
+                        System.out.println("Lock in wrong");
+                        c.lock.V();
+                    }
                 }
             } else if (message.charAt(0) == MessageTypes.Selection.Message) {
                 if(message.length() == 1) return;
@@ -247,6 +256,7 @@ class CommunicationThread implements Runnable {
                 if (selectedCoupon != null && selectedCoupon.getHash().equals(hash)) selectedCoupon = null;
                 Coupon c = App.coupons.get(hash);
                 if (c != null && c.lock != null) {
+                    System.out.println("V in dismiss");
                     c.lock.V();
                 }
             } else if (message.charAt(0) == MessageTypes.OwnUserData.Message) {
